e52ec1495cbbc07207de803f229730dfa998cfdb
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2022-02-02 21:07:52 +0530

    Added simple component for table header & body and using props for data mapping

--- react-study-app/src/Table.js
+++ react-study-app/src/Table.js
@@ -1,24 +1,38 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = (props) => {
+  const rows = props.displayData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
+    );
+  });
+  return <tbody>{rows}</tbody>;
+};
 
 class Table extends Component {
-    render(){
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Pranay</td>
-                        <td>Software Developer</td>
-                    </tr>
-                </tbody>
-            </table>
-        )
-    }
+  render() {
+    const { displayData } = this.props;
+    return (
+      <table>
+        <TableHeader></TableHeader>
+        <TableBody displayData={displayData}></TableBody>
+      </table>
+    );
+  }
 }
 
 export default Table;
