9a6deb07f45b23b021d67d553ca3f3dc9f2cee8b
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2022-02-02 21:06:27 +0530

    Showing display data received as prop

--- react-study-app/src/App.js
+++ react-study-app/src/App.js
@@ -1,14 +1,15 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Table from "./Table";
+import displayData from "./data";
 class App extends Component {
-    render() {
-      return (
-        <div className="App">
-          <h1>Hello React</h1>
-          <Table></Table>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello React</h1>
+        <Table displayData={displayData}></Table>
+      </div>
+    );
   }
+}
 
-  export default App
+export default App;
