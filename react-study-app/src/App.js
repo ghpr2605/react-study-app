d8fe7cdfc1fd748de762e943bdeaba89dee868c5
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2022-02-07 20:17:43 +0530

    Added logic to delete data and using state for data storing

--- react-study-app/src/App.js
+++ react-study-app/src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from "react";
 import Table from "./Table";
 import displayData from "./data";
 class App extends Component {
+  state = {
+    persons: displayData,
+  };
+
+  removePersonData = (index) => {
+    const { persons } = this.state;
+    this.setState({
+      persons: persons.filter((person, i) => i !== index),
+    });
+  };
+
   render() {
+    const { persons } = this.state;
     return (
       <div className="App">
-        <h1>Hello React</h1>
-        <Table displayData={displayData}></Table>
+        <Table
+          displayData={persons}
+          removePersonData={this.removePersonData}
+        ></Table>
       </div>
     );
   }
